4fd374135036cd8dfe4d126b51720abf623c07f3
3lis3 <elise.vanaerschot@hotmail.com>
2022-12-23 11:58:28 +0100

    Basis - added button with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DoneComponent } from './done/done.component';
+import { OpenComponent } from './open/open.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/open', pathMatch: 'full' },
+  { path: 'open', component: OpenComponent },
+  { path: 'done', component: DoneComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
